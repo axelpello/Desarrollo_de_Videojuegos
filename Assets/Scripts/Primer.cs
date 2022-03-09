c7d143c9b86d9c06ce6975b3c9d12de8ade34c14
axelpello <axelpello@hotmail.com>
2022-03-09 08:59:27 -0300

    Primer commit
    Carga del proyecto de Unity adentro del repositorio.

---
+++ Assets/Scripts/Primer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Primer : MonoBehaviour
+{
+    public Vector3 escalaCubo;          //Defino variables
+    public Vector3 movimientoCubo;
+    public GameObject elCubo;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+      movimientoCubo = new Vector3 (0f, 0f, 0.01f);       //Le inicializo el valor a ambas variables
+      escalaCubo = new Vector3 (3, 5, 2);
+      elCubo.transform.localScale = escalaCubo;   //Al apretar start, cambia la escala del cubo por el valor de "Escala Cubo"
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      elCubo.transform.position += movimientoCubo;  //Le digo que le sume 1 sobre el eje Z en cada frame para que se mueva constantemente a esa velocidad.
+    }
+}
